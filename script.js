392304a38704853f18b1e11315f3c1f1a1beb993
ashvinsunga <ashvinsunga@yahoo.com.ph>
2021-09-06 19:45:56 +0800

    done

--- script.js
+++ script.js
@@ -25,11 +25,11 @@ function search(arr, val){
     let max = arr.length - 1; 
 
     while (min <= max) { 
-        let middle = Math.floor((min + max) / 2); // 2
-        let currentElement = arr[middle]; // [3]
+        let middle = Math.floor((min + max) / 2); 
+        let currentElement = arr[middle]; 
 
-        if (currentElement < val) { //                     
-            min = middle + 1; // 4                 [1,2,3,4,5,6], 6
+        if (currentElement < val) {                     
+            min = middle + 1;             
         }
         else if (currentElement > val){ 
             max = middle - 1;  
